ec74ad77d08d543fd2bbd2e3b008f0e6e7a6e524
Kateryna2882 <govoryhina82@gmail.com>
2024-02-18 13:53:00 +0200

    Add tests and fields

--- src/main/java/com/example/springio/entity/Product.java
+++ src/main/java/com/example/springio/entity/Product.java
@@ -3,11 +3,13 @@ package com.example.springio.entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 @Data
 @AllArgsConstructor
 @Builder
+@NoArgsConstructor
 public class Product {
     @Id
     private int id;
